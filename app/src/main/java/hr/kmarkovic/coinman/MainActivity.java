446288608dff010755862f9a09eaf90c1be24687
Kristian Marković <kmarkovic.dev@gmail.com>
2016-12-01 09:56:21 +0100

    Initial commit.

---
+++ app/src/main/java/hr/kmarkovic/coinman/MainActivity.java
@@ -0,0 +1,13 @@
+package hr.kmarkovic.coinman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
